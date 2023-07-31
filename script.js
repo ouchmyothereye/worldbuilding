37bc89e0136edc55580b625344cbcfa9ede42119
ouchmyothereye <ouchmyothereye@gmail.com>
2023-07-30 21:54:33 -0700

    total change

--- script.js
+++ script.js
@@ -1,112 +1,35 @@
 let data;
-let groups = {
-    "nation": [
-        "community_tags",
-        "nation_currentprobs",
-        "nation_disputes",
-        "nation_goodthings",
-        "nation_positiveties",
-        "nation_themes",
-        "history_conclusion",
-        "history_crisis",
-        "history_events",
-        "history_origin",
-        "history_originals",
-        "history_peak",
-        "history_surviviors",
-        "history_zenith"
-    ]
-    // Add more groups as needed
-};
-let savedResults = [];
 
+// Load the JSON file when the page loads
 window.onload = function() {
     fetch('combined.json')
         .then(response => response.json())
         .then(json => {
             data = json;
-            loadTables();
         });
 }
 
-function loadTables() {
-    const groupsSelect = document.getElementById('groups');
-    for (let group in groups) {
-        const option = document.createElement('option');
-        option.value = group;
-        option.textContent = group;
-        groupsSelect.appendChild(option);
-    }
+// Generate a random nation
+function generateNation() {
+    const nationThemes = getRandomOption('nation_themes');
+    const nationCurrentProbs = getRandomOption('nation_currentprobs');
+    const nationDisputes = getRandomOption('nation_disputes');
+    const nationGoodThings = getRandomOption('nation_goodthings');
+    const nationPositiveties = getRandomOption('nation_positiveties');
+    const historyEvents1 = getRandomOption('history_events');
+    const historyEvents2 = getRandomOption('history_events');
 
-    const checkboxesDiv = document.getElementById('checkboxes');
-    for (let table in data) {
-        const group = table.split('_')[0];
-        let groupDiv = document.getElementById(group);
-        if (!groupDiv) {
-            groupDiv = document.createElement('div');
-            groupDiv.id = group;
-            groupDiv.className = 'group';
-            groupDiv.innerHTML = `<h2>${group}</h2>`;
-            checkboxesDiv.appendChild(groupDiv);
-        }
+    const result = `This polity is ${nationThemes}. While ${nationGoodThings}, ${nationCurrentProbs}. In the past ${historyEvents1} as well as ${historyEvents2}. On the diplomatic side of things, ${nationDisputes}.`;
 
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.id = table;
-        checkbox.value = table;
-
-        const label = document.createElement('label');
-        label.htmlFor = table;
-        label.textContent = table;
-
-        groupDiv.appendChild(checkbox);
-        groupDiv.appendChild(label);
-        groupDiv.appendChild(document.createElement('br'));
-    }
-}
-
-function selectGroup() {
-    const group = document.getElementById('groups').value;
-    if (group) {
-        const tables = groups[group];
-        for (let table in data) {
-            const checkbox = document.getElementById(table);
-            checkbox.checked = tables.includes(table);
-        }
-    }
-}
-
-function generateRandomOptions() {
     const resultsDiv = document.getElementById('results');
-    resultsDiv.textContent = '';
+    resultsDiv.textContent = result;
+}
 
-    for (let table in data) {
-        const checkbox = document.getElementById(table);
-        if (checkbox.checked) {
-            const randomOption = data[table][Math.floor(Math.random() * data[table].length)];
-            const result = `${table}: ${randomOption}`;
-
-            const p = document.createElement('p');
-            p.textContent = result;
-            resultsDiv.appendChild(p);
-
-            const saveButton = document.createElement('button');
-            saveButton.textContent = 'Save';
-            saveButton.onclick = function() { saveResult(result); };
-            resultsDiv.appendChild(saveButton);
-        }
+// Get a random option from a table
+function getRandomOption(table) {
+    if (table in data) {
+        return data[table][Math.floor(Math.random() * data[table].length)];
+    } else {
+        return '';
     }
-}
-
-function saveResult(result) {
-    savedResults.push(result);
-}
-
-function downloadSavedResults() {
-    const blob = new Blob([savedResults.join('\n')], {type: 'text/plain'});
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = 'saved_results.txt';
-    a.click();
 }
